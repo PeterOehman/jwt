02eb15fb43924f1fc26d3f832fc26a2aadde551a
Peter Oehman <tucker2923@gmail.com>
2022-01-11 12:01:11 -0500

    basibasic server setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const posts = [
+  {
+    username: 'Peter',
+    title: 'Post 1'
+  },
+  {
+    username: 'Peter2',
+    title: 'Post 2'
+  }
+]
+
+app.get('/posts', (req, res) => {
+  res.json(posts)
+})
+
+app.get('/login', (req, res) => {
+   
+})
+
+app.listen(3000)
